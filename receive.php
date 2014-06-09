fdf019047dd02921deff3a00bc11fe786cebf956
Keith Casey <keith@caseysoftware.com>
2014-06-09 16:12:50 -0500

    updating the code to remove the unneeded parameters

--- receive.php
+++ receive.php
@@ -4,15 +4,7 @@ include 'vendor/autoload.php';
 
 $apiKey = 'update with your api key';
 
-// The preg_replace just filters the to/from information to make sure we're only dealing with numbers.
-//   It may be overkill in this case but better safe than sorry.
-$to = $_POST['To'];
-$to = preg_replace("/[^0-9]/", "", $to);
-
-$from = $_POST['From'];
-$from = preg_replace("/[^0-9]/", "", $from);
-
-$recording_url = $_POST['RecordingUrl'];
+$recording_url = $_POST['RecordUrl'];
 
 $jsonData = '{"to": "'.$to.'", "from": "'.$from.'"}';
 
