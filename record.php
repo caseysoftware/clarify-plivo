96450c2decbe88bb8b5ba88c8e9efce01588ff3e
Keith Casey <keith@caseysoftware.com>
2014-06-09 15:14:28 -0500

    updating the xml to match the syntax

--- record.php
+++ record.php
@@ -1,12 +1,5 @@
-<?php
-
-include 'vendor/autoload.php';
-
-$response = new Services_Twilio_Twiml();
-$response->say('Hello. This call will be recorded for quality assurance.');
-$response->record(array(
-    'action' => 'receive.php',
-    'maxLength' => 20
-));
-
-print $response;
+<Response>
+    <Speak>Hello. This call will be recorded for quality assurance.</Speak>
+    <Record action="http://www.example.com/receive.php" maxLength="60" finishOnKey="*" />
+    <Speak>Recording not received</Speak>
+</Response>
