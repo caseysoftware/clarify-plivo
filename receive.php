4e01a5359e0c8c4d4cf4c77afa111eda50aca82e
Keith Casey <keith@caseysoftware.com>
2014-06-12 12:35:11 -0500

    updating the fields to get start/end date of recordings

--- receive.php
+++ receive.php
@@ -6,7 +6,10 @@ $apiKey = 'update with your api key';
 
 $recording_url = $_POST['RecordUrl'];
 
-$jsonData = '{"to": "'.$to.'", "from": "'.$from.'"}';
+$recording_start = (int) $_POST['RecordingStartMs'];
+$recording_end = (int) $_POST['RecordingEndMs'];
+
+$jsonData = '{"started": "'.date('Y-m-d H:i:s', $recording_start).'", "ended": "'.date('Y-m-d H:i:s', $recording_end).'"}';
 
 $bundle = new OP3Nvoice\Bundle($apiKey);
 $bundle->create($recording_url, 'phone recording', '', '', $jsonData);
